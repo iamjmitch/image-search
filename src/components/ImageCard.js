a050d8460f4d2c87a62c9635bb72750114b7da50
James Mitchell <iamjmitch1990@gmail.com>
2020-02-26 12:09:45 +1000

    Update ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -23,11 +23,7 @@ class ImageCard extends React.Component {
   render() {
     const { description, urls } = this.props.image;
     return (
-        <div
-            style={{
-          height: 'auto',
-          gridRowEnd: `span ${this.state.spans}`
-        }}>
+        <div>
           <img ref={this.imageRef} alt={description} src={urls.regular} />
         </div>
     );
